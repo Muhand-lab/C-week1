3cddf28672106dfbf20bf15d83e91ae8deb82b40
Hamzah Al Mokdad <h.al.mokdad@st.hanze.nl>
2025-11-29 21:04:42 +0100

    Fix formatting issues in week1-2opdracht2.c

--- week1-2opdracht2.c
+++ week1-2opdracht2.c
@@ -1,32 +1,36 @@
-//week1-2opdra2.c (versie voor opdracht 2)
+
+// week1-2opdra2.c (versie voor opdracht 2)
+
 #include <stdio.h>
+
 void print_function(char letter);
+
 int main(void)
 {
     char star = '*';
     char dash = '-';
     int row;
-    for (row=0 ; row < 10; row++)
+
+    for (row = 0; row < 10; row++)
     {
         print_function(star);
         printf(" ");
         print_function(dash);
         printf("\n");
-
     }
+
     return 0;
-
-
-
-
-
-
 }
+
 void print_function(char letter)
 {
     int i;
-    for(i = 0; i < 9; i++)
+
+    for (i = 0; i < 9; i++)
     {
         printf("%c", letter);
     }
 }
+```
+
+Sources
