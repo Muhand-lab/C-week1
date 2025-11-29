0915a766589878beb5aae4eb982a0405ae232a9d
Hamzah Al Mokdad <h.al.mokdad@st.hanze.nl>
2025-11-29 21:07:30 +0100

    Fix input format specifier and correct spelling

--- week1-4.c
+++ week1-4.c
@@ -1,23 +1,28 @@
-
 #include <stdio.h>
 
 void calculate(double diameter, double *circumference);
+
 int main(void)
 {
     double diameter;
     double circumference;
-    printf("Geef de dimater van de cirkel: ");
-    if (scanf("%1F", &diameter) != 1)
+
+    printf("Geef de diameter van de cirkel: ");
+
+    if (scanf("%lf", &diameter) != 1)
     {
-        printf("Ongeldige invoer. \n");
+        printf("Ongeldige invoer.\n");
         return 1;
     }
 
-     calculate(diameter, &circumference);
-    printf("De omterk van een cirkel met diameter %.2f is %.2f\n",
-          diameter, circumference);
+    calculate(diameter, &circumference);
+
+    printf("De omtrek van een cirkel met diameter %.2f is %.2f\n",
+           diameter, circumference);
+
     return 0;
 }
+
 void calculate(double diameter, double *circumference)
 {
     const double PI = 3.14159265358979323846;
