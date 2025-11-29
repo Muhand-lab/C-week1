6f57ee054424e9898456618923577a9384bba798
Hamzah Al Mokdad <h.al.mokdad@st.hanze.nl>
2025-11-29 21:13:28 +0100

    Refactor circle circumference calculation

--- week1-4.c
+++ week1-4.c
@@ -1,25 +1,38 @@
-
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void calculate(double diameter, double *circumference);
+double circumference_from_diameter(double d)
+{
+    double pi = 3.141592653589793;
+    return pi * d;
+}
+
 int main(void)
 {
+    char buf[100];
     double diameter;
-    double circumference;
-    printf("Geef de dimater van de cirkel: ");
-    if (scanf("%1F", &diameter) != 1)
-    {
-        printf("Ongeldige invoer. \n");
+    double omtrek;
+
+    printf("Geef de diamter van de cirkel (bv 5.2): ");
+
+    if (fgets(buf, sizeof buf, stdin) == NULL) {
+        puts("Hmm, niks ingevoerd. Ik stop even.");
         return 1;
     }
 
-     calculate(diameter, &circumference);
-    printf("De omterk van een cirkel met diameter %.2f is %.2f\n",
-          diameter, circumference);
-    return 0;
-}
-void calculate(double diameter, double *circumference)
-{
-    const double PI = 3.14159265358979323846;
-    *circumference = PI * diameter;
-}
+    if (sscanf(buf, "%lf", &diameter) != 1) {
+        printf("Huh? kon geen nummer lezen. Probeer iets zoals 3.5\n");
+        return 1;
+    }
+
+    if (diameter < 0) {
+        printf("Negatieve diameter? Ok, ik gebruik de absolute waarde.\n");
+        diameter = fabs(diameter);
+    }
+
+    omtrek = circumference_from_diameter(diameter);
+    printf("Dus ehm... de omtrek van een cirkel met diameter %.2f is ongeveer %.2f\n",
+           diameter, omtrek);
+
+    return 0;}
